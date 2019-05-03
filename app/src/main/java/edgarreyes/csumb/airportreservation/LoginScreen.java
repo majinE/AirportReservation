83755dd1a4a99edbe5586c7f55b04572df45f4ec
Edgar Reyes <edgreyes@csumb.edu>
2019-05-03 15:54:37 -0700

    Initial commit

---
+++ app/src/main/java/edgarreyes/csumb/airportreservation/LoginScreen.java
@@ -0,0 +1,37 @@
+package edgarreyes.csumb.airportreservation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginScreen extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    EditText username;
+    EditText password;
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+
+        username = findViewById(R.id.userNameEditText);
+        password = findViewById(R.id.passwordEditText);
+        login = findViewById(R.id.loginButton);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String usernameLogged = username.getText().toString();
+                String passwordLogged = password.getText().toString();
+                Log.i(TAG, "username: " + usernameLogged);
+                Log.i(TAG, "password: " + passwordLogged);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/edgarreyes/csumb/airportreservation/MainScreen.java
@@ -0,0 +1,55 @@
+package edgarreyes.csumb.airportreservation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainScreen extends AppCompatActivity {
+
+    TextView mainMessage;
+    Button createAccount;
+    Button reserveSeat;
+    Button cancelReservation;
+    Button manageSystem;
+    final String reserveSeatLogin = "reserve";
+    final String cancelReserveLogin = "cancel";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+        createAccount = findViewById(R.id.CreateAccountButton);
+        reserveSeat = findViewById(R.id.ReserveSeatButton);
+        cancelReservation = findViewById(R.id.CancelReservationButton);
+        manageSystem = findViewById(R.id.ManageSystemButton);
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createAcc = new Intent(MainScreen.this, CreateAccount.class);
+                startActivity(createAcc);
+            }
+        });
+
+        reserveSeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(MainScreen.this, LoginScreen.class);
+                login.putExtra("SessionID", reserveSeatLogin);
+                startActivity(login);
+            }
+        });
+
+        cancelReservation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent login = new Intent(MainScreen.this, LoginScreen.class);
+                login.putExtra("SessionID", cancelReserveLogin);
+            }
+        });
+    }
+}
